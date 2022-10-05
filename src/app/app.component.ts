5418742d8736decb2f29be427ca6025e6b32f449
Sourya Mishra <sourya.mishra111@gmail.com>
2022-10-06 01:16:01 +0530

    Add reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'template-driven-form';
+  title = 'angular-forms';
+  currentFormType = 'reactive';
+  formTypes = ['reactive', 'template', 'dynamic'];
+
+  setFormType(formType: string): void {
+    if (this.formTypes.includes(formType)) {
+      this.currentFormType = formType;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroFormComponent],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  declarations: [AppComponent, HeroFormComponent, NameEditorComponent, ProfileEditorComponent],
+  imports: [BrowserModule, CommonModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
