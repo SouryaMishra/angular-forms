7ebad560bcc8fa63b3df7273e664f8dfee85ea7c
Sourya Mishra <sourya.mishra@educationsoftwaresolutions.co.uk>
2022-10-07 18:37:14 +0530

    add question control service

---
+++ src/app/question-control.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { QuestionBase } from './question-base';
+
+@Injectable()
+export class QuestionControlService {
+  constructor() {}
+
+  toFormGroup(questions: QuestionBase<string>[]) {
+    // const group: any = questions.reduce((prev, question) => {
+    //   prev[question.key] = question.required
+    //     ? new FormControl(question.value || '', Validators.required)
+    //     : new FormControl(question.value || '');
+    // }, {});
+
+    const group: any = {};
+
+    questions.forEach((question) => {
+      group[question.key] = question.required
+        ? new FormControl(question.value || '', Validators.required)
+        : new FormControl(question.value || '');
+    });
+
+    return new FormGroup(group);
+  }
+}
