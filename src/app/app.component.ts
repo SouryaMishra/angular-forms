6c3598adb61349815ce75c4c28521e50edd6b4a2
Sourya Mishra <sourya.mishra111@gmail.com>
2022-10-08 12:08:34 +0530

    pass questions as input and render DynamicForm component inside AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { QuestionBase } from './question-base';
+import { QuestionService } from './question.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [QuestionService],
 })
 export class AppComponent {
   title = 'angular-forms';
   currentFormType = 'reactive';
   formTypes = ['reactive', 'template', 'dynamic'];
+  questions$: Observable<QuestionBase<string>[]>;
+
+  constructor(private questionService: QuestionService) {
+    this.questions$ = this.questionService.getQuestions();
+  }
 
   setFormType(formType: string): void {
     if (this.formTypes.includes(formType)) {
